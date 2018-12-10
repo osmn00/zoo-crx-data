18d18af2d15246e3bc885c7b7757dceb4537a87e
堂主 <machong@cai-inc.com>
2018-12-10 09:33:03 +0800

    x

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var util = require('util');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const util = require('util');
+const querystring = require('querystring');
 
-var mimetype = {
-  'txt': 'text/plain',
-  'html': 'text/html',
-  'css': 'text/css',
-  'xml': 'application/xml',
-  'json': 'application/json',
-  'js': 'application/javascript',
-  'jpg': 'image/jpeg',
-  'jpeg': 'image/jpeg',
-  'gif': 'image/gif',
-  'png': 'image/png',
-  'svg': 'image/svg+xml'
+let mimetype = {
+    'txt': 'text/plain',
+    'html': 'text/html',
+    'css': 'text/css',
+    'xml': 'application/xml',
+    'json': 'application/json',
+    'js': 'application/javascript',
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'gif': 'image/gif',
+    'png': 'image/png',
+    'svg': 'image/svg+xml'
 };
 
-var page_404 = function(req, res, path){
+let page_404 = function (req, res, path) {
     res.writeHead(404, {
-      'Content-Type': 'text/html'
+        'Content-Type': 'text/html'
     });
     res.write('<!doctype html>\n');
     res.write('<title>404 Not Found</title>\n');
     res.write('<h1>Not Found</h1>');
     res.write(
-    '<p>The requested URL ' +
-     path +
-    ' was not found on this server.</p>'
+        '<p>The requested URL ' +
+        path +
+        ' was not found on this server.</p>'
     );
     res.end();
 };
 
-var page_500 = function(req, res, error){
+let page_500 = function (req, res, error) {
 
     res.writeHead(500, {
-      'Content-Type': 'text/html'
+        'Content-Type': 'text/html'
     });
     res.write('<!doctype html>\n');
     res.write('<title>Internal Server Error</title>\n');
@@ -46,27 +47,35 @@ var page_500 = function(req, res, error){
 
 http.createServer(function (req, res) {
 
-    var pathname = url.parse(req.url).pathname;
-    var realPath = __dirname +  "/static" + pathname;
+    let pathname = url.parse(req.url).pathname;
+    let realPath = __dirname + "/static" + pathname;
+    let _callback = querystring.parse(url.parse(req.url).query).callback;
 
-    fs.exists(realPath, function(exists){
-	if(!exists){
+    console.log(_callback, res.write.bind(res))
 
-	    return page_404(req, res, pathname);
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            return page_404(req, res, pathname);
+        } else {
+            let file = fs.createReadStream(realPath);
 
-	} else {
-	    var file = fs.createReadStream(realPath);
+            res.writeHead(200, {
+                'Content-Type': mimetype[realPath.split('.').pop()] || 'text/plain'
+            });
 
-    	    res.writeHead(200, {
-       		'Content-Type': mimetype[realPath.split('.').pop()] || 'text/plain'
-    	    });
+            if (_callback) {
+                file.on('data', function (data) {
+                    res.end(_callback + '(' + data.toString() + ')')
+                });
+            } else {
+                file.on('data', res.write.bind(res));
+            }
 
-	    file.on('data', res.write.bind(res));
-	    file.on('close', res.end.bind(res));
-	    file.on('error', function(err){
-		return page_500(req, res, err);
-	    });
-	}
+            file.on('close', res.end.bind(res));
+            file.on('error', function (err) {
+                return page_500(req, res, err);
+            });
+        }
     });
 }).listen(1337);
 
